be7159a8ca7102522f44c79bd7edf915c31e845e
amirabugaball <97474861+amirabugaball@users.noreply.github.com>
2023-01-17 19:10:00 +0200

    Delete lib directory

--- lib/models/addresses/delivery_locations.dart
+++
@@ -1,9 +0,0 @@
-class DeliveryLocationsClass {
-  String id;
-  String name;
-  double deliveryFees;
-  int deliveryTime;
-
-  DeliveryLocationsClass(
-      this.id, this.name, this.deliveryFees, this.deliveryTime);
-}
--- lib/models/main/main_values.dart
+++
@@ -1,9 +0,0 @@
-class MainValues {
-  static List orderStatus = [
-    'Placed',
-    'In the kitchen',
-    'On the way',
-    'Delivered!',
-    'Canceled'
-  ];
-}
